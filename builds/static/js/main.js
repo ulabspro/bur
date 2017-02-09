ca9bc5d5f40999a8b7d263208bbd94779db13e17
extiser <1sappho@gmail.com>
2017-02-09 13:21:52 +0500

    fix

--- builds/static/js/main.js
+++ builds/static/js/main.js
@@ -2,22 +2,22 @@ jQuery(document).ready(function($) {
 	$('.ie8 .clients-list__item:nth-child(5)').css('margin-left', '205px');
 });
 
-// $(window).scroll(function(event) {
-// 	var scrollTop = $(document).scrollTop();
-// 	if (scrollTop > 10) {
-// 		$('#header').addClass('fixed');
-// 	} else {
-// 		$('#header').removeClass('fixed');
-// 	}
-// });
-// jQuery(document).ready(function($) {
-// 	$('.ie8 .header-menu__item:nth-child(3)').css('margin-right', '160px');
-// 	$('.ie8 .header-menu__item:nth-child(4)').css('margin-left', '160px');
+$(window).scroll(function(event) {
+	var scrollTop = $(document).scrollTop();
+	if (scrollTop > 10) {
+		$('#header').addClass('fixed');
+	} else {
+		$('#header').removeClass('fixed');
+	}
+});
+jQuery(document).ready(function($) {
+	$('.ie8 .header-menu__item:nth-child(3)').css('margin-right', '160px');
+	$('.ie8 .header-menu__item:nth-child(4)').css('margin-left', '160px');
 
-// 	$('#mobile-menu').click(function(event) {
-// 		$(this).next().slideToggle(400).toggleClass('active');
-// 	});
-// });
+	$('#mobile-menu').click(function(event) {
+		$(this).next().slideToggle(400).toggleClass('active');
+	});
+});
 
 jQuery(document).ready(function($) {
 	$('.form__radio-group').find('.radio').each(function() {
--- markup/modules/header/header.js
+++ markup/modules/header/header.js
@@ -1,16 +1,16 @@
-// $(window).scroll(function(event) {
-// 	var scrollTop = $(document).scrollTop();
-// 	if (scrollTop > 10) {
-// 		$('#header').addClass('fixed');
-// 	} else {
-// 		$('#header').removeClass('fixed');
-// 	}
-// });
-// jQuery(document).ready(function($) {
-// 	$('.ie8 .header-menu__item:nth-child(3)').css('margin-right', '160px');
-// 	$('.ie8 .header-menu__item:nth-child(4)').css('margin-left', '160px');
+$(window).scroll(function(event) {
+	var scrollTop = $(document).scrollTop();
+	if (scrollTop > 10) {
+		$('#header').addClass('fixed');
+	} else {
+		$('#header').removeClass('fixed');
+	}
+});
+jQuery(document).ready(function($) {
+	$('.ie8 .header-menu__item:nth-child(3)').css('margin-right', '160px');
+	$('.ie8 .header-menu__item:nth-child(4)').css('margin-left', '160px');
 
-// 	$('#mobile-menu').click(function(event) {
-// 		$(this).next().slideToggle(400).toggleClass('active');
-// 	});
-// });
+	$('#mobile-menu').click(function(event) {
+		$(this).next().slideToggle(400).toggleClass('active');
+	});
+});
